1c4bcc564ad70ce463694d06bfff95e8cd071cbc
gallen <gallen.coding@gmail.com>
2018-03-11 20:31:03 +0000

    first try

--- app/src/main/java/com/packtpub/workoutscheduler/newRoutine.java
+++ app/src/main/java/com/packtpub/workoutscheduler/newRoutine.java
@@ -1,13 +1,36 @@
 package com.packtpub.workoutscheduler;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class newRoutine extends AppCompatActivity {
+public class newRoutine extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_routine);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_list,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_insert:
+                newExercise();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
